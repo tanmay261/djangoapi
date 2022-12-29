af47b29dbe2ad70028040eed71abd6a672206ee6
Tanmay <tanmaytanu.10@gmail.com>
2022-12-29 20:20:13 +0530

    Permissions , Admin , list_display added

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from api.models import Company,Employee
 # Register your models here.
+
+# LEARN
+# To display only specific columns for Companies
+class CompanyAdmin(admin.ModelAdmin):
+    list_display=('name','location','type')
+
+
+# LEARN
+# To display only specific columns for Employees
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('name','position','company')
+
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Employee,EmployeeAdmin)
