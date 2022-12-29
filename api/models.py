dafd572a2578d10b248d0bffa7269738071beebc
Tanmay <tanmaytanu.10@gmail.com>
2022-12-29 14:09:10 +0530

    Employee Model

--- api/models.py
+++ api/models.py
@@ -10,3 +10,14 @@ class Company(models.Model):
     type=models.CharField(max_length=100,choices=(("IT","IT"),("Non-IT","Non-IT"), ("Mobiles","Mobiles") ))
     added_date=models.DateTimeField(auto_now=True)
     active=models.BooleanField(default=True)
+
+#Creating Employee Model
+
+class Employee(models.Model):
+    name=models.CharField(max_length=100)
+    email_id=models.CharField(max_length=50)
+    address=models.CharField(max_length=200)
+    phone=models.CharField(max_length=10)
+    about=models.TextField()
+    position=models.CharField(max_length=50,choices=(("manager","Manager"),("Software Developer", "SD"),("Project Leader ","PL")))
+    
