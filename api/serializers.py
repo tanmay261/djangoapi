a867e68a5af76c085253551ceb1df43542896a59
Tanmay <tanmaytanu.10@gmail.com>
2022-12-29 16:33:17 +0530

    Employee Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
-from api.models import Company
+from api.models import Company,Employee
 #Creating Seralizers 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    # Generally id is hidden so we expose it and make it a READ ONLY Column
     company_id=serializers.ReadOnlyField()
+    # Meta generally means data about some data
     class Meta:
         model=Company
+        fields="__all__"
+
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    # Generally id is hidden so we expose it and make it a READ ONLY Column
+    id=serializers.ReadOnlyField()
+    # Meta generally means data about some data
+    class Meta:
+        model=Employee
         fields="__all__"
         
 
