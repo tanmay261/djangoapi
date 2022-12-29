1f04798d72288e0be5fe06eac7a858ef144e8b5f
Tanmay <tanmaytanu.10@gmail.com>
2022-12-29 16:36:59 +0530

    Revert "view set for Employee"
    This reverts commit 8dc72bd76fa9a68fc6f5b07777b891abd6aa8d7a.

--- api/views.py
+++ api/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from api.models import Company,Employee
-from api.serializers import CompanySerializer,EmployeeSerializer
+from api.models import Company
+from api.serializers import CompanySerializer
 # Create your views here.
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset=Company.objects.all()
     serializer_class=CompanySerializer
-
-class EmployeeViewSet(viewsets.ModelViewSet):
-    queryset=Employee.objects.all()
-    serializer_class=EmployeeSerializer
